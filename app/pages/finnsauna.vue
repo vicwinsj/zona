7a6b580bcd4b440357533af7d94c79aed03e6eaf
piidun <tonyarntsen91@gmail.com>
2025-09-19 21:48:59 +0200

    Add a temp sauna

--- app/pages/finnsauna.vue
+++ app/pages/finnsauna.vue
@@ -1,23 +1,31 @@
 <template>
-    <div style="height:100vh; width:100vw">
-      <LMap
-        ref="map"
-        :zoom="zoom"
-        :center="[47.21322, -1.559482]"
-        :use-global-leaflet="false"
-      >
-        <LTileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-          layer-type="base"
-          name="OpenStreetMap"
-        />
-      </LMap>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  const zoom = ref(6)
-  </script>
-  
+  <div style="height:100vh; width:100vw">
+    <LMap
+      ref="map"
+      :zoom="zoom"
+      :center="[47.21322, -1.559482]"
+      :use-global-leaflet="false"
+    >
+      <LTileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors"
+        layer-type="base"
+        name="OpenStreetMap"
+      />
+
+      <!-- Add a marker at specific coordinate -->
+      <LMarker :lat-lng="[63.763255, 10.070788]">
+        <LPopup>
+          <span>Victor's hot sauna</span>
+        </LPopup>
+      </LMarker>
+    </LMap>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { LMap, LTileLayer, LMarker, LPopup } from '@vue-leaflet/vue-leaflet'
+
+const zoom = ref(6)
+</script>
